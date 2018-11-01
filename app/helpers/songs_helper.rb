038cf27709e2324aa50a590a66ecaca92da34b06
Adam Urbina <adamurbina@gmail.com>
2018-11-01 14:26:15 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,13 @@
 module SongsHelper
+  def display_artist(song)
+    binding.pry
+    if song.artist
+      puts("reached artsit_path branch")
+      artist_path(song.artist)
+    else
 
+      puts("reached nil")
+      edit_song_path(song)
+    end
+  end
 end
