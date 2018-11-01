ffbc9c11501fa3dc048229d8719346f0e4672add
Adam Urbina <adamurbina@gmail.com>
2018-11-01 14:18:44 +0000

    automatically backed up by learn

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,8 +2,10 @@ module ArtistsHelper
 
   def display_artist(song)
     if song.artist.nil?
+      puts("reached nil")
       edit_song_path(song)
     else
+      puts("reached artsit_path branch")
       artist_path(song.artist)
     end
   end
